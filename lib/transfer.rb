e0906b0e695a4036ed701389ef08326f062f8079
Adrian Daras <getpingui@gmail.com>
2017-03-16 08:05:25 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,39 @@
 class Transfer
-  # your code here
+  attr_reader :amount, :receiver, :sender, :status
+
+  def initialize(sender, receiver, amount)
+    @amount = amount
+    @receiver = receiver
+    @sender = sender
+    @status = "pending"
+  end
+
+  def execute_transaction
+    if valid? && sender.balance >= amount && @status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      @status = "complete"
+    else
+      reject_transfer
+    end
+  end
+
+  def reject_transfer
+    @status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
+
+  def reverse_transfer
+    if valid? && receiver.balance >= amount && @status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
+      @status = "reversed"
+    else
+      reject_transfer
+    end
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
 end
