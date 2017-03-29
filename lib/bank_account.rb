9544fa4d688f19b3687f837a18096c6153239137
Ilya Lavandoso <ultraprolid@gmail.com>
2017-03-29 11:21:07 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,38 @@
 class BankAccount
+  attr_accessor :balance,:status
+  attr_reader :name
 
+  def initialize name
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit money
+    @balance+=money
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def close_account
+    @status = "closed"
+  end
+
+  def valid?
+    valid_status? && valid_deposit?
+  end
+
+  def valid_sender? amount
+    valid? && @balance >= amount
+  end
+
+  def valid_status?
+    @status == "open"
+  end
+
+  def valid_deposit?
+    @balance > 0
+  end
 end
